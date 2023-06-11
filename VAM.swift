a52a9436a7c68959e4355b118bd5f46efc6e506c
insub <vb15@kakao.com>
2023-06-11 22:48:54 +0900

    Add files via upload

---
+++ VAM.swift
@@ -0,0 +1,41 @@
+//
+//  VAM.swift
+//  TEST
+//
+//  Created by 김인섭 on 2023/06/10.
+//
+
+import SwiftUI
+import Combine
+
+// MARK: VAS
+public protocol VAM: View {
+    
+    associatedtype Action: Actionable
+    associatedtype Model: Modelable
+    
+    @MainActor var action: Action { get }
+    @MainActor var model: Model { get }
+}
+
+// MARK: Action
+public protocol Actionable: AnyObject {
+    
+    associatedtype Model
+    
+    @MainActor var model: Model? { get }
+}
+
+@MainActor class Action<T: Model>: Actionable {
+    
+    weak var model: T?
+    
+    init(model: T?) {
+        self.model = model
+    }
+}
+
+// MARK: Model
+public protocol Modelable: AnyObject { }
+
+@MainActor class Model: ObservableObject, Modelable { }
